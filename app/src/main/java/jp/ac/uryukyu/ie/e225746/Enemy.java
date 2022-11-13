73d8d14fccbae76552759a7f935f2f899e77e7c2
keito <e225746@ie.u-ryukyu.ac.jp>
2022-11-13 22:01:18 +0900

    step3

--- app/src/main/java/jp/ac/uryukyu/ie/e225746/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225746/Enemy.java
@@ -9,16 +9,17 @@ package jp.ac.uryukyu.ie.e225746;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
      * @param name モンスター名
      * @param maximumHP モンスターのHP
      * @param attack モンスターの攻撃力
+     * @param dead モンスターの生死
      */
     public Enemy (String name, int maximumHP, int attack) {
         this.name = name;
@@ -28,6 +29,19 @@ public class Enemy {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+    public boolean getDead(){
+        return this.dead;
+    }
     /**
      * Heroへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
@@ -36,7 +50,7 @@ public class Enemy {
     public void attack(Hero hero){
         if (this.dead == false){
             int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
         }
     }
