f3c3a47342c7588e619a47e9a982142843d35a36
keito <e225746@ie.u-ryukyu.ac.jp>
2022-12-03 20:48:01 +0900

    Heroから継承するように修正

--- app/src/main/java/jp/ac/uryukyu/ie/e225746/Warrior.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225746/Warrior.java
@@ -1,6 +1,6 @@
 package jp.ac.uryukyu.ie.e225746;
 
-public class Warrior extends LivingThing{
+public class Warrior extends Hero{
     public Warrior(String _name, int _maximumHP, int _attack){
         super(_name,_maximumHP,_attack); 
     }
@@ -12,14 +12,4 @@ public class Warrior extends LivingThing{
             livingthing.wounded(damage);
         }
     }
-
-    @Override
-    public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("%sは道半ばで力尽きてしまった。\n", name);
-        }
-    }
-
 }
